8256fbbf51626e4ae5a46d3173b9ee17c57a09c0
Andrew peters <apeters07@outlook.com>
2017-12-23 15:30:24 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetector : MonoBehaviour
+{
+	public float MovingForce;
+	Vector3 StartPoint;
+	Vector3 Origin;
+	public int NoOfRays = 10;
+	int i;
+	RaycastHit HitInfo;
+	float LengthOfRay, DistanceBetweenRays, DirectionFactor;
+	float margin = 0.015f;
+	Ray ray;
+
+	void Start ()
+	{
+		//Length of the Ray is distance from center to edge
+		LengthOfRay = GetComponent<Collider>().bounds.extents.y;
+		//Initialize DirectionFactor for right direction
+		DirectionFactor = Mathf.Sign (Vector3.right.x);
+
+	}
+
+	void Update ()
+	{
+		// First ray origin point for this frame
+		StartPoint = new Vector3 (GetComponent<Collider>().bounds.min.y + margin, transform.position.y, transform.position.z);
+		if (!IsCollidingHorizontally ()) {
+			transform.Translate (Vector3.right * MovingForce * Time.deltaTime * DirectionFactor);
+		}
+	}
+
+	bool IsCollidingHorizontally ()
+	{
+		Origin = StartPoint;
+		DistanceBetweenRays = (GetComponent<Collider>().bounds.size.y - 2 * margin) / (NoOfRays - 1);
+		for (i = 0; i<NoOfRays; i++) {
+			// Ray to be casted.
+			ray = new Ray (Origin, Vector3.right * DirectionFactor);
+			//Draw ray in scene view to see visually. Remember visual length is not actual length
+			Debug.DrawRay (Origin, Vector3.right * DirectionFactor, Color.yellow);
+			if (Physics.Raycast (ray, out HitInfo, LengthOfRay)) {
+				print ("Collided With " + HitInfo.collider.gameObject.name);
+				// Negate the Directionfactor to reverse the moving direction of colliding cube(here cube2)
+				DirectionFactor = -DirectionFactor;
+				return true;
+			}
+			Origin += new Vector3 (0, DistanceBetweenRays, 0);
+		}
+		return false;
+
+	}
+
+
+}
---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+	/** 	Objective
+	 *		- Get the enemy moving forward and backward
+	 *		- When the enemy collides with a "Wall" its direction is reversed
+	 */
+
+	public float walkSpeed;
+	public Vector2 moveVector;
+	public bool isGrounded;
+
+	void Start() {
+		walkSpeed = 6.0f;
+		moveVector = new Vector2(6.0f, 0f);
+		isGrounded = true;
+	}
+
+	void Update() {
+		if(moveVector.x >= 0) {
+			// sprite change (facing right)
+		}
+		else {
+			// ''''''''''' face left
+		}
+		if(!isGrounded) {
+			transform.Translate(moveVector.x * Time.deltaTime, -1.0f * Time.deltaTime, 0f);
+		}
+		transform.Translate(moveVector.x * Time.deltaTime, 0f, 0f);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Wall")) {
+			// reverse the direction of the enemy
+			Debug.Log("Hit wall");
+			moveVector = new Vector2(-1*moveVector.x, 0f);
+		}
+		/*			 isGrounded = true;
+		 }
+		 else {
+			 isGrounded = false;
+		 } */
+	}
+}
