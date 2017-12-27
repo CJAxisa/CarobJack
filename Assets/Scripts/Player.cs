47fba0eeb3db408d5097aabd454f049f3e5edc05
spalunki <axisac72@gmail.com>
2017-12-27 17:19:35 -0500

    got player-enemy collision detecting, next push, i'll get the knock back working

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -31,23 +31,33 @@ public class Player : MonoBehaviour {
 
     private void checkForEnemies()
     {
+        /*
         //goes through each enemy and does AABB (Axis Aligned Bounding Box) to check ffor collisions.
         foreach (GameObject enem in enemies)
         {
-            
-            if (transform.position.x - gameObject.GetComponent<SpriteRenderer>().bounds.size.x * 0.5f < enem.transform.position.x + enem.GetComponent<SpriteRenderer>().bounds.size.x * 0.5f &&
-                transform.position.x + gameObject.GetComponent<SpriteRenderer>().bounds.size.x * 0.5f > enem.transform.position.x - enem.GetComponent<SpriteRenderer>().bounds.size.x * 0.5f &&
-                transform.position.y + gameObject.GetComponent<SpriteRenderer>().bounds.size.y * 0.5f > enem.transform.position.y - enem.GetComponent<SpriteRenderer>().bounds.size.y * 0.5f &&
-                transform.position.y - gameObject.GetComponent<SpriteRenderer>().bounds.size.y * 0.5f < enem.transform.position.y + enem.GetComponent<SpriteRenderer>().bounds.size.y * 0.5f
+            /*
+             * This will check if;
+             * box B's x min is less than box a's x max
+             * box B's x max is greater than box a's x min
+             * box B's y min is less than box a's y max
+             * box B's y max is greater than box a's y min
+             * 
+             * if all of these are true, then we have a collision
+             
+            if (gameObject.GetComponent<Collider2D>().bounds.min.x < enem.GetComponent<Collider2D>().bounds.max.x &&
+                gameObject.GetComponent<Collider2D>().bounds.min.x > enem.GetComponent<Collider2D>().bounds.max.x &&
+                gameObject.GetComponent<Collider2D>().bounds.min.y > enem.GetComponent<Collider2D>().bounds.max.y &&
+                gameObject.GetComponent<Collider2D>().bounds.min.y < enem.GetComponent<Collider2D>().bounds.max.y
                )
             {
                 Debug.Log(enem);
 
             }
         }
+    */
     }
 
-    public void OnCollisionEnter2D(Collision2D co)
+    public void OnCollisionEnter(Collision co)
     {
         Debug.Log("AAAAAAAA");
         if (co.gameObject.CompareTag("Enemy")) 
