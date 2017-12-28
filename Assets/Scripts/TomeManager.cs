d82b4a260271697e7e3481e1c351942236281eb2
Nick Vallejos <nvallejos1234@gmail.com>
2017-12-27 21:25:31 -0500

    *We can now cycle through tomesgit add Assets/Scenes/nickScene.unity Assets/Scripts/TomeManager.cs Assets/Scripts/StunTome.cs Assets/Scripts/StunTome.cs.metaadd Assets/Scenes/nickScene.unity Assets/Scripts/TomeManager.cs Assets/Scripts/StunTome.cs Assets/Scripts/StunTome.cs.meta (Also added Stun Tome so check out nick's scene)

--- Assets/Scripts/TomeManager.cs
+++ Assets/Scripts/TomeManager.cs
@@ -5,14 +5,13 @@ using Tomes;
 
 /** This is the "inventory"
  *	This script will be attatched to the player object
- *	This script handles tome selection
+ *	This script handles tome selection using a list
  */
 public class TomeManager : MonoBehaviour {
   private List<Tome> inventory;
   private int tomeIndex;
 
   private Tome current;
-	private Tome other;
 	private bool inUse;
   // keeps track of current item - index
   // general tome object = Tome tome;
@@ -34,62 +33,78 @@ public class TomeManager : MonoBehaviour {
   void Start() {
     inventory = new List<Tome>();
     tomeIndex = 0;
+
+		AddTome(gameObject.GetComponent<FireTome>());
+		AddTome(gameObject.GetComponent<StunTome>());
+
 		current = gameObject.GetComponent<FireTome>(); // <--- THIS IS HOW YOU CHANGE THE TOME OBJECT TYPE
-		other = current;
 		inUse = false;
   }
 
   void Update() {
-    //pressing q decreases tomeIndex, if the index is the first tome it will warp to the last index
+    /* pressing 'q' decreases tomeIndex, if the index is the first tome it will warp to the last index */
     if(Input.GetKeyDown("q")) {
       if(inventory.Count > 0) {
-  		  if(tomeIndex == 0) {
-  			  tomeIndex = inventory.Count - 1;
+		    current.use(false);
+			  if(tomeIndex == 0) {
+  		    tomeIndex = inventory.Count - 1;
   		  }
   		  else {
-  			  tomeIndex -= 1;
+  		    tomeIndex -= 1;
   		  }
+			  current = inventory[tomeIndex];
+			  /* IF YOU WANT IT SO THAT WAY YOU CAN SWAP BETWEEN TOMES AND CONTINUE USING THE TOMES UNCOMMENT THIS BLOCK BELOW */
+			  /*
+			  if(Input.GetMouseButton(0)) {
+			  	 current.use(true);
+			  }
+			  */
       }
 	  }
-    //pressing e increases tomeIndex, if the index is the last tome it will warp to the first index
+
+		/* pressing 'e' increases tomeIndex, if the index is the last tome it will warp to the first index */
 	  if(Input.GetKeyDown("e")) {
 		  if(inventory.Count > 0) {
+			  current.use(false);
   		  if(tomeIndex == inventory.Count - 1) {
-  			  tomeIndex = 0;
+  				tomeIndex = 0;
   		  }
   		  else {
           tomeIndex += 1;
   		  }
       }
+			current = inventory[tomeIndex];
+			/* IF YOU WANT IT SO THAT WAY YOU CAN SWAP BETWEEN TOMES AND CONTINUE USING THE TOMES UNCOMMENT THIS BLOCK BELOW */
+			/*
+			if(Input.GetMouseButton(0)) {
+				current.use(true);
+			}
+			*/
 	  }
-	 //to use tome
-	 /* Could use mouse click */
+
+	  /* Could use mouse click */
    /*if(Input.GetMouseButtonDown(0)) {
      // tome.use();
      // snapping for fire tome will go into that method, NOT here
      // this is bc each tome has different behaviour - for example jump tome
      // does not require aiming. no reason to execute that code when it's not needed
      Debug.Log("Used Tome!");
-   } */
-	 if(Input.GetMouseButtonDown(0)) {
-		 inUse = true;
-		 //current.use(inUse);
-		 other.use(inUse);
-		 Debug.Log("Used Tome!");
-	 }
-	 if(Input.GetMouseButtonUp(0)) {
-		 inUse = false;
-		 //current.use(inUse);
-		 other.use(inUse);
-	 }
+     } */
 
-	  Debug.Log(tomeIndex);
+	   /* To use tomes */
+	   if(Input.GetMouseButtonDown(0)) {
+		   current.use(true);
+	   }
+	   if(Input.GetMouseButtonUp(0)) {
+		   current.use(false);
+	   }
+
+	    //Debug.Log(tomeIndex);
   }
 
   void AddTome(Tome newTome) {
     inventory.Add(newTome);
-    //currentTomeName = newTome;
-	  /* then set currentTomeName (inventory[index] to false)*/
+    //current = newTome; <----- This makes your new tome the newly acquired tome
   }
   /* We need something for selection */
 	/* Here we will keep track of all the tomes the player has */
