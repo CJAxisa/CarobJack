c8025c2726bc8eeb48eabe50991ae4bb454fda56
Nick Vallejos <nvallejos1234@gmail.com>
2018-05-25 14:51:44 -0400

    Sample scene with basic physics controller script; basic tile palletes

---
+++ Assets/scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraMovement : MonoBehaviour {
+	public Transform target;
+
+  public float smoothSpeed = 0.125f;
+  public Vector3 offset;
+
+	void LateUpdate() {
+		Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, target.position.z) + offset;
+    transform.position = desiredPosition;
+	}
+
+
+}
